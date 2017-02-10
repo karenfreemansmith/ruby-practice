ba0fc6f100fbbd30ebc00eabe30fbe71e6fb669f
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-02-10 07:38:37 -0800

    print balance

--- account.rb
+++ account.rb
@@ -17,9 +17,21 @@ class BankAccount
     @transactions.push(description: description, amount: amount)
   end
 
+  def balance
+    balance=0
+    @transactions.each do|transaction|
+      balance += transaction[:amount]
+    end
+    return balance
+  end
+
+  def to_s
+    "Name: #{@name}, Balance: $#{sprintf("%0.2f", balance)}"
+  end
+
 end
 
 bank_account = BankAccount.new("Karen")
 bank_account.credit("Paycheck", 1250)
 bank_account.debit("Groceries", 150)
-puts bank_account.inspect
+puts bank_account
